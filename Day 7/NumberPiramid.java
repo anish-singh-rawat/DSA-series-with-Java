ef38a3fc9782ce9b3a7c574540008f6049de83bf
anish-singh-rawat <www.computernetwork@gmail.com>
2024-09-17 02:12:13 +0530

    create palendrom pattern

---
+++ Day 7/NumberPiramid.java
@@ -0,0 +1,17 @@
+public class NumberPiramid {
+    public static void printNumberPiramid(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <num-i;j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2*i)-1; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumberPiramid(5);
+    }
+}
---
+++ Day 7/PalendromPiramid.java
@@ -0,0 +1,20 @@
+public class PalendromPiramid {
+    public static void printPalendromPiramid(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printPalendromPiramid(5);
+    }
+}
